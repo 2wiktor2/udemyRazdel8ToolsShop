f69d20d6115af7d61e12421d68fb734b3455e57e
wiktor <wiktor@bk.ru>
2021-07-16 17:56:45 +0300

    lesson_52 pause at 6:41

--- app/src/main/java/com/wiktor/udemyrazdel8toolsshop/MainActivity.java
+++ app/src/main/java/com/wiktor/udemyrazdel8toolsshop/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wiktor.udemyrazdel8toolsshop;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
 
     }
 }
