5c4000298a8180085d385558a21f31e0a0206cb2
wiktor <wiktor@bk.ru>
2021-07-20 00:19:31 +0300

    lesson_53 init

--- app/src/main/java/com/wiktor/udemyrazdel8toolsshop/DrillCategoryActivity.java
+++ app/src/main/java/com/wiktor/udemyrazdel8toolsshop/DrillCategoryActivity.java
@@ -48,6 +48,7 @@ public class DrillCategoryActivity extends AppCompatActivity {
                 intent.putExtra("resId", drill.getImageResourceId());
                 startActivity(intent);
             }
+
         });
 
     }
