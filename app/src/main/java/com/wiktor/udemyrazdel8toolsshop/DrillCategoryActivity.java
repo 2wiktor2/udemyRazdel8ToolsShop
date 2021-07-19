b85714892cc82e7fd195b99d96cd4628883a8b33
wiktor <wiktor@bk.ru>
2021-07-19 23:53:16 +0300

    lesson_53 pause

--- app/src/main/java/com/wiktor/udemyrazdel8toolsshop/DrillCategoryActivity.java
+++ app/src/main/java/com/wiktor/udemyrazdel8toolsshop/DrillCategoryActivity.java
@@ -1,14 +1,36 @@
 package com.wiktor.udemyrazdel8toolsshop;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DrillCategoryActivity extends AppCompatActivity {
+
+    private ListView listViewDrills;
+
+    private ArrayList<Drill> drills;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drill_category);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        listViewDrills = findViewById(R.id.listViewDrills);
+
+        drills = new ArrayList<>();
+
+        ArrayAdapter<Drill> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, drills);
+        listViewDrills.setAdapter(adapter);
+
     }
 }
