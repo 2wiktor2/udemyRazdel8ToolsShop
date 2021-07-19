7fc3ee6a376f47c1e3dfcd39bad5a2fee357fd8e
wiktor <wiktor@bk.ru>
2021-07-20 00:17:55 +0300

    lesson_53 finish

--- app/src/main/java/com/wiktor/udemyrazdel8toolsshop/DrillCategoryActivity.java
+++ app/src/main/java/com/wiktor/udemyrazdel8toolsshop/DrillCategoryActivity.java
@@ -3,7 +3,10 @@ package com.wiktor.udemyrazdel8toolsshop;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -29,8 +32,23 @@ public class DrillCategoryActivity extends AppCompatActivity {
 
         drills = new ArrayList<>();
 
+        drills.add(new Drill(getString(R.string.drill_interskol_title), getString(R.string.drill_interskol_info), R.drawable.interskol));
+        drills.add(new Drill(getString(R.string.drill_makita_title), getString(R.string.drill_makita_info), R.drawable.makita));
+        drills.add(new Drill(getString(R.string.drill_dewalt_title), getString(R.string.drill_dewalt_title), R.drawable.dewalt));
+
         ArrayAdapter<Drill> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, drills);
         listViewDrills.setAdapter(adapter);
+        listViewDrills.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Drill drill = drills.get(position);
+                Intent intent = new Intent(getApplicationContext(), DrillDetailActivity.class);
+                intent.putExtra("title", drill.getTitle());
+                intent.putExtra("info", drill.getInfo());
+                intent.putExtra("resId", drill.getImageResourceId());
+                startActivity(intent);
+            }
+        });
 
     }
 }
