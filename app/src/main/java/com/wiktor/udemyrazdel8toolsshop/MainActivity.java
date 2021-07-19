c9892ae8b770fa11e3e0ac02d2ef8997b0211e74
wiktor <wiktor@bk.ru>
2021-07-19 11:52:47 +0300

    lesson_52 finished

--- app/src/main/java/com/wiktor/udemyrazdel8toolsshop/MainActivity.java
+++ app/src/main/java/com/wiktor/udemyrazdel8toolsshop/MainActivity.java
@@ -3,9 +3,16 @@ package com.wiktor.udemyrazdel8toolsshop;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewTools;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +20,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null){
+        if (actionBar != null) {
             actionBar.hide();
         }
+
+        listViewTools = findViewById(R.id.listViewTools);
+        listViewTools.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //--- выводим тост с номером позиции. getApplicationContext() - Вместо this получае контекст приложения, т.к. мы сейчас находимся не в активности, а в новом классе OnItemClickListener()
+                Toast.makeText(getApplicationContext(), "Позиция : " + position, Toast.LENGTH_SHORT).show();
+                switch (position) {
+                    case 0:
+                        Intent intent = new Intent(getApplicationContext(), DrillCategoryActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
 
     }
 }
